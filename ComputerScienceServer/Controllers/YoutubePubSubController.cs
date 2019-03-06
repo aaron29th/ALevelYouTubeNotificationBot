49b14347c0f29d441a081a667794f1a37c7572d3
UnverifiedEmu <aaron29th@gmail.com>
2019-03-06 19:47:50 +0000

    init

---
+++ ComputerScienceServer/Controllers/YoutubePubSubController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ComputerScienceServer.Models;
+using Discord;
+using Discord.Webhook;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerScienceServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YoutubePubSubController : ControllerBase
+    {
+        [HttpPost]
+        public string Post([FromBody] PubSubFeed pubSubFeed)
+        {
+	        ulong id = 467813391742926868;
+	        string token = "***REMOVED***";
+
+	        Embed embeded;
+
+	        DiscordWebhookClient client = new DiscordWebhookClient(id, token);
+	        client.SendMessageAsync();
+
+			return pubSubFeed.Title;
+        }
+    }
+}
--- ComputerScienceServer/Startup.cs
+++ ComputerScienceServer/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +27,11 @@ namespace ComputerScienceServer
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+			services.AddMvc(options =>
+			{
+				options.InputFormatters.Add(new XmlSerializerInputFormatter(options));
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
