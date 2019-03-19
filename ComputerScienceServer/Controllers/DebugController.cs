16ac2f7fa27fe0bad5a531ba4576814ffee199c3
1 <aaron29th@gmail.com>
2019-03-19 21:28:44 +0000

    Fixed twitter oauth

--- ComputerScienceServer/Controllers/DebugController.cs
+++ ComputerScienceServer/Controllers/DebugController.cs
@@ -21,18 +21,12 @@ namespace ComputerScienceServer.Controllers
 			_context = context;
 		}
 
-		[HttpGet("TestPage")]
-		public ActionResult TestPage()
-		{
-			return Ok("Hello world");
-		}
-
-		[HttpGet("TestPage2")]
+		[HttpGet("LogError")]
 		public ActionResult TestPage2()
 		{
 			_context.ErrorLog.Add(new ErrorLog()
 			{
-				ExceptionMessage = "An error occured",
+				ExceptionMessage = "An error occured - test",
 				Location = "Debug"
 			});
 			_context.SaveChanges();
@@ -40,63 +34,20 @@ namespace ComputerScienceServer.Controllers
 			return Ok(_context.ErrorLog.First());
 		}
 
-		[HttpGet("{id}")]
-		public async Task<ActionResult> Get(string id)
+		[HttpGet("SendHelloWorldTweet")]
+		public string SendHelloWorldTweet()
 		{
-			int result = 0;
-			switch (id)
+			string twitterToken = "***REMOVED***";
+			string twitterSecret = "***REMOVED***";
+
+			TwitterService service = new TwitterService(Config.TwitterConsumerKey, Config.TwitterConsumerSecret);
+
+			service.AuthenticateWith(twitterToken, twitterSecret);
+			service.SendTweet(new SendTweetOptions()
 			{
-				case "newTwitterUser":
-					TwitterUser user = new TwitterUser()
-					{
-						Token = "***REMOVED***",
-						TokenSecret = "***REMOVED***",
-						TweetTemplate = "{{AuthorName}} published a new video {{VideoTitle}}"
-					};
-					await _context.TwitterUsers.AddAsync(user);
-					result = await _context.SaveChangesAsync();
-					return Ok($"Save changes {result}");
-
-				case "newDiscordWebhook":
-					Webhook webhook = new Webhook()
-					{
-						Id = 467813391742926868,
-						Token = "***REMOVED***",
-						MessageTemplate = null,
-						EmbedTemplate = "{\"content\":\"this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\\nfunction foo(bar) {\\n  console.log(bar);\\n}\\n\\nfoo(1);```\",\"embed\":{\"title\":\"title ~~(did you know you can have markdown here too?)~~\",\"description\":\"this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\\nyes, even code blocks```\",\"url\":\"https://discordapp.com\",\"color\":437808,\"timestamp\":\"2019-03-07T22:22:01.330Z\",\"footer\":{\"icon_url\":\"https://cdn.discordapp.com/embed/avatars/0.png\",\"text\":\"footer text\"},\"thumbnail\":{\"url\":\"https://cdn.discordapp.com/embed/avatars/0.png\"},\"image\":{\"url\":\"https://cdn.discordapp.com/embed/avatars/0.png\"},\"author\":{\"name\":\"author name\",\"url\":\"https://discordapp.com\",\"icon_url\":\"https://cdn.discordapp.com/embed/avatars/0.png\"}}}"
-					};
-					await _context.Webhooks.AddAsync(webhook);
-					result = await _context.SaveChangesAsync();
-					return Ok(result);
-			}
-
-			return Ok();
-		}
-
-		// This URL is registered as the application's callback
-		// at http://dev.twitter.com
-		[HttpGet("AuthorizeCallback")]
-		public ActionResult AuthorizeCallback([FromQuery] string oauth_token, 
-			[FromQuery] string oauth_verifier)
-		{
-			var requestToken = new OAuthRequestToken { Token = oauth_token };
-
-			// Step 3 - Exchange the Request Token for an Access Token
-			TwitterService service = new TwitterService(
-				Config.TwitterConsumerKey, Config.TwitterConsumerSecret);
-
-			OAuthAccessToken accessToken = service.GetAccessToken(requestToken, 
-				oauth_verifier);
-
-			// Step 4 - User authenticates using the Access Token
-			service.AuthenticateWith(accessToken.Token, 
-				accessToken.TokenSecret);
-
-			TweetSharp.TwitterUser user = service.VerifyCredentials(
-				new VerifyCredentialsOptions());
-
-			string username = user.ScreenName;
-			return Ok(username);
+				Status = "Hello world..."
+			});
+			return "";
 		}
 	}
 }
--- ComputerScienceServer/Controllers/TwitterController.cs
+++ ComputerScienceServer/Controllers/TwitterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ComputerScienceServer.Models;
+using ComputerScienceServer.Models.Twitter;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TweetSharp;
@@ -20,60 +21,50 @@ namespace ComputerScienceServer.Controllers
 		    _context = context;
 	    }
 
-		[HttpGet("SendHelloWorld")]
-        public string SendHelloWorld()
-        {
-	        string twitterToken = "***REMOVED***";
-	        string twitterSecret = "***REMOVED***";
-
-			TwitterService service = new TwitterService(Config.TwitterConsumerKey, Config.TwitterConsumerSecret);
-
-	        service.AuthenticateWith(twitterToken, twitterSecret);
-	        service.SendTweet(new SendTweetOptions()
-	        {
-				Status = "Hello world..."
-	        });
-			return "";
-        }
+		
 
         [HttpGet("GetOauthLink")]
         public ActionResult GetOauthLink()
         {
-			// Step 1 - Retrieve an OAuth Request Token
-			TwitterService service = new TwitterService(
-				Config.TwitterConsumerKey, Config.TwitterConsumerSecret);
+			//Retrieve an OAuth Request Token
+			TwitterService service = new TwitterService(Config.TwitterConsumerKey, Config.TwitterConsumerSecret);
 
-			// This is the registered callback URL
-			OAuthRequestToken requestToken = service.GetRequestToken(
-				Config.TwitterCallbackUrl);
+			OAuthRequestToken requestToken = service.GetRequestToken(Config.TwitterCallbackUrl);
 
-			// Step 2 - Redirect to the OAuth Authorization URL
+			//Generate oauth link
 			Uri uri = service.GetAuthorizationUri(requestToken);
 			return Ok(uri.ToString());
 		}
 
         [HttpGet("OauthCallback")]
         // ReSharper disable twice InconsistentNaming
-        public ActionResult OauthCallback([FromQuery] string oauth_token, [FromQuery] string oauth_verifier)
+        public async Task<ActionResult> OauthCallback([FromQuery] string oauth_token, 
+	        [FromQuery] string oauth_verifier)
         {
 	        var requestToken = new OAuthRequestToken { Token = oauth_token };
 
-	        // Step 3 - Exchange the Request Token for an Access Token
-	        TwitterService service = new TwitterService(
-		        Config.TwitterConsumerKey, Config.TwitterConsumerSecret);
+	        var service = new TwitterService(Config.TwitterConsumerKey, Config.TwitterConsumerSecret);
 
-	        OAuthAccessToken accessToken = service.GetAccessToken(requestToken,
-		        oauth_verifier);
+			//Get access token
+	        var accessToken = service.GetAccessToken(requestToken, oauth_verifier);
 
-	        // Step 4 - User authenticates using the Access Token
-	        service.AuthenticateWith(accessToken.Token,
-		        accessToken.TokenSecret);
+			//Check auth was successful
+	        if (String.IsNullOrWhiteSpace(accessToken.ScreenName)) return BadRequest();
 
-	        TweetSharp.TwitterUser user = service.VerifyCredentials(
-		        new VerifyCredentialsOptions());
+	        //User authenticates using the Access Token
+	        //service.AuthenticateWith(accessToken.Token, accessToken.TokenSecret);
+	        //string username = user.ScreenName;
+			//TweetSharp.TwitterUser user = service.VerifyCredentials(new VerifyCredentialsOptions());
 
-	        string username = user.ScreenName;
-	        return Ok(username);
+			await _context.TwitterUsers.AddAsync(new Models.Twitter.TwitterUser()
+	        {
+				Token =  accessToken.Token,
+				TokenSecret = accessToken.Token,
+				Name = accessToken.ScreenName
+	        });
+			await _context.SaveChangesAsync();
+	        
+	        return NoContent();
         }
     }
 }
