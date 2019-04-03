3f8a045edc34433a22d0c62182abd0a704553910
1 <aaron29th@gmail.com>
2019-04-03 14:05:33 +0100

    Changed config

--- ComputerScienceServer/Startup.cs
+++ ComputerScienceServer/Startup.cs
@@ -33,8 +33,6 @@ namespace YoutubeNotifyBot
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			Config.Init();
-
 			//Add jwt token authentication
 			var symmetricSecurityKey = new SymmetricSecurityKey(Config.JwtSecurityKey);
 			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
